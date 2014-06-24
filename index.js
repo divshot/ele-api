142ee0b26dc8cfd13cda61a0c69a4b2e2c1e2589
Scott Corgan <scottcorgan@gmail.com>
2014-06-24 09:40:35 -0700

    basic routing organization

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ var logger = require('./lib/logger');
 var PORT = process.env['PORT'] || 3000;
 var express = require('express');
 var session = require('cookie-session');
-var passport = require('passport');
-var GitHubStrategy = require('passport-github').Strategy;
-
 var app = module.exports = express();
+
 app.use(session({
   secret: process.env['SESSION_SECRET'] || 'yEkWdTDGin2ajoCbxzuEeDOZzLVoy8BM4tH7S_R2'
 }));
@@ -15,8 +13,15 @@ app.use(logger.network());
 
 var auth = require('./lib/auth')(app);
 
-app.get('/', auth.authorize(), function (req, res) {
-  res.send(req.user);
+[
+  'packages',
+  'user-packages',
+  'home'
+].forEach(function (routeName) {
+  require('./lib/routes/' + routeName)({
+    app: app,
+    auth: auth
+  });
 });
 
 app.listen(PORT, function() {
