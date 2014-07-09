430934fb3c5f26c8d25a53c1d19a91df12e7a6ac
Scott Corgan <scottcorgan@gmail.com>
2014-07-09 14:26:56 -0700

    tests: full test coverage for Github model

--- test/models/github.js
+++ test/models/github.js
@@ -53,26 +53,84 @@ describe('model: Github', function () {
     
     describe('.getUserByUsername(username, callback)', function () {
       it('gets user data from Github by the Github login', function (done) {
-        
-        // TODO: write this test
-        
-        done();
-      });
-    });
-    
-    
-    
-    describe('.createGist(data, callback)', function () {
-      it('makes a request to createa gist on Github', function (done) {
-        gh.createGist({name: 'name'}, function (err, body) {
-          expect(body.headers).to.contain.keys(Object.keys(gh.headers()).map(function (key) { return key.toLowerCase(); }));
-          expect(body.method).to.equal('POST');
-          expect(body.url).to.equal('/gists');          
+        gh.getUserByUsername('ele', function (err, res) {
+          expect(res.method).to.equal('GET');
+          expect(res.url.indexOf('/users/ele')).to.equal(0);
+          expectGithubAuthHeaders(res, gh);
           done();
         });
       });
     });
     
+    describe('.getUserByAccessToken(callback)', function () {
+      it('gets the user', function (done) {
+        gh.getUserByAccessToken(function (err, res) {
+          expect(res.method).to.equal('GET');
+          expect(res.url).to.equal('/user');
+          expectGithubAuthHeaders(res, gh);
+          done();
+        });
+      });
+    });
+    
+    describe('.createGist(data, callback)', function () {
+      it('makes a request to createa gist on Github', function (done) {
+        gh.createGist({name: 'name'}, function (err, res) {
+          expect(res.method).to.equal('POST');
+          expect(res.url).to.equal('/gists');          
+          expectGithubAuthHeaders(res, gh);
+          done();
+        });
+      });
+    });
+    
+    describe('.updateGist(id, data, callback)', function () {
+      it('updates the gist on Github', function (done) {
+        gh.updateGist(123, {}, function (err, res) {
+          
+          // TODO: figure out how to test json body data
+          // with Mocksy
+          
+          expect(res.method).to.equal('PATCH');
+          expect(res.url).to.equal('/gists/123');
+          expectGithubAuthHeaders(res, gh);
+          done();
+        });
+      });
+    });
+    
+    describe('.getGistFileContentsForCommit(id, sha, callback)', function () {
+      it('requests and parses the files in a gist on Github', function (done) {
+        gh._request = function (options, done) {
+          options.files = ['files'];
+          var response = JSON.stringify(options);
+          done(null, {body: response}, response);
+        };
+        
+        gh.getGistFileContentsForCommit(123, 456, function (err, files) {
+          expect(files).to.eql(['files']);
+          done();
+        });
+      });
+    });
+    
+    describe('._request(options, done)', function () {
+      it('makes an http request with options', function (done) {
+        gh._request({
+          url: localhost + '/testing',
+          method: 'POST'
+        }, function (err, response, body) {
+          body = JSON.parse(body);
+          expect(body.url).to.equal('/testing');
+          expect(body.method).to.equal('POST');
+          done();
+        });
+      });
+    });
+    
+    function expectGithubAuthHeaders (res, gh) {
+      expect(res.headers).to.contain.keys(Object.keys(gh.headers()).map(function (key) { return key.toLowerCase(); }));
+    }
   });
   
   describe('Github', function () {
