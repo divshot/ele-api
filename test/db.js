8953f4fc9f526c8d2f744305809682b76a253625
Scott Corgan <scottcorgan@gmail.com>
2014-07-10 21:31:43 -0700

    tests: coverage for db

--- test/db.js
+++ test/db.js
@@ -1,6 +1,16 @@
+require('mockgoose')(require('mongoose'));
+
 var expect = require('chai').expect;
 var db = require('../lib/db');
 
 describe('DB', function () {
+  it('connects to the database', function () {
+    expect(require('mongoose').connection).to.not.equal(undefined);
+  });
   
+  it('throws an error when the database emits and error', function () {
+    expect(function () {
+      db.emit('error', 'error');
+    }).to.throw('Cannot connect to database. error');
+  });
 });
